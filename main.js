db6b0b21d3f56a09a17b825728b9a888246d52dd
Jorge Cabrera <joca220725@gmail.com>
2023-11-10 23:42:59 -0500

    Detalles del producto agregado, no funciona al 100%

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ const menuMobile = document.querySelector('.mobile-menu');
 const shoppingIcon = document.querySelector('.navbar-shopping-cart');
 const shoppingMenu = document.querySelector('.product-detail');
 const cardsContainer = document.querySelector('.cards-container');
+//Detalles de producto
+const closeIconDetail = document.querySelector('.product-detail-close');
+const aside = document.querySelector('.product-detail2')
 
 shoppingIcon.addEventListener('click',toggleShoppingMenu);
 menuEmail.addEventListener('click', toggleDestopMenu);
@@ -14,6 +17,8 @@ hamburIco.addEventListener('click', toggleMobileMenu);
 function toggleDestopMenu(){
     shoppingMenu.classList.add('inactive');
     desktopMenu.classList.toggle('inactive');
+    // cerrar la ventana de detallesdel producto cuando abra el menu de escritorio
+    aside.classList.add('inactive');
 }
 
     // Esta funcion pone o quita la clase que esta dentro del los parentesis
@@ -26,11 +31,15 @@ function toggleDestopMenu(){
  function toggleMobileMenu(){
     shoppingMenu.classList.add('inactive');
     menuMobile.classList.toggle('inactive');
+    // cerrar la ventana de detallesdel producto cuando abra el menu mobile
+    aside.classList.add('inactive');
  }
  function toggleShoppingMenu(){
     desktopMenu.classList.add('inactive');
     menuMobile.classList.add('inactive');
     shoppingMenu.classList.toggle('inactive');
+    // cerrar la ventana de detallesdel producto cuando abra el menu de compras
+    aside.classList.add('inactive');
  }
 
 // lista de Carrito de compras
@@ -38,22 +47,40 @@ function toggleDestopMenu(){
 const productList = [];
 
 productList.push({
-    name: "Jbl",
-    price: 200,
-    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    name: "Bmw x4M",
+    price: "200.000",
+    image: "https://www.bmw.ca/content/dam/bmw/common/all-models/m-series/x4m/2019/inspire/bmw-x4m-inspire-mg-m-competition-desktop-03.jpg",
+    description: "El BMW X4 es un automóvil todoterreno de lujo del segmento D con carrocería cupé similar al BMW X6 producido por el fabricante de automóviles alemán BMW desde el año 2014. Comparte plataforma con el BMW X3 el cual a su vez se basa en el BMW Serie 3."
 });
 productList.push({
-    name: "Gorra gooring",
-    price: 80,
-    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    name: "Kawassaki z1000",
+    price: "40.000",
+    image: "https://0.cdn.autotraderspecialty.com/Motorcycle-200902913-5ee400c070a9ac48912f958bfad145fb.jpg",
+    id: 'kawassaki'
 });
 
 productList.push({
-    name: 't-shirt',
-    price: 88,
-    image:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-    
+    name: 'Can-am Maverick x3',
+    price: "50.000",
+    image:"https://utvactionmag.com/wp-content/uploads/2017/12/Can-Am2_WDP_2161_Maverick-X3-Intro_Mexico_.jpg",
+    description: "Sin duda es el campeón de las rocas, la tierra y las dunas. El Maverick X3 se adapta a cualquier aventura que sueñes. Empaca y toma el camino para llegar a los campamentos más remotos, o ganar la carrera más difícil."
 });
+productList.push({
+    name:'Bmw M2 competition' ,
+    price: "90.000",
+    image: "https://i2-prod.mirror.co.uk/incoming/article7423906.ece/ALTERNATES/s1200d/P90199694_highRes_the-new-bmw-m2-coupe.jpg",
+    description: "The M2 Competition uses the BMW M GmbH S55 engine, a high performance variant of the N55 engine, that has been detuned from its application in the F80 M3 and F82 M4"
+});
+
+productList.push({
+    name: 'Jet Ski',
+    price: '15.000',
+    image: 'https://th.bing.com/th/id/R.0eb2b878fef0601b3e206256955a44ff?rik=qpEM9SFgqm6%2b7Q&riu=http%3a%2f%2fwww.rockypoint-vacations.com%2fwp-content%2fuploads%2f2011%2f04%2f12653_1.jpg&ehk=IPms7NUspEVGJnePJXscsIfmGpODQdI1oWfyCsA8TQY%3d&risl=&pid=ImgRaw&r=0',
+    description: 'Jet Ski es una marca de motos acuáticas fabricadas por Kawasaki Heavy Industries. El nombre, sin embargo, se ha convertido en un nombre común usado para cualquier tipo de moto acuática. '
+});
+
+
+
 // se hace un ciclo para mostrar productos de manera dinamica
 
 function mostrarProductos(arr){
@@ -90,6 +117,140 @@ function mostrarProductos(arr){
         productCard.appendChild(mainImage);//Imagen dentro del contenedor principal
         productCard.appendChild(productInfo);//Product info dentro del contenedor principal
         cardsContainer.appendChild(productCard);//Todo dentro de cardsContainer
+        productCard.addEventListener('click', (arr)=>{
+            const aside = document.querySelector('.product-detail2')
+
+            const imagenProduct = document.querySelector('.product-detail2-img');
+            imagenProduct.setAttribute('src', product.image);
+            
+            const nameProduct = document.querySelector('.product-detail2-name');
+            nameProduct.innerText= product.name;
+
+            const priceProduct = document.querySelector('.product-detail2-price');
+            priceProduct.innerText = product.price;
+
+            const descriptionProduct = document.querySelector('.product-detail2-description');
+            descriptionProduct.innerText = product.description;
+
+            aside.classList.toggle('inactive');
+            closeIconDetail.addEventListener('click', ()=>{aside.classList.add('inactive')});
+
+        } )
     }
 }
 mostrarProductos(productList);
+
+
+
+// function detallesProductos(arr){
+//     for (prodct of arr){
+//     const asideProducDetail2 = document.createElement('aside');
+//     asideProducDetail2.classList.add('product-detail2 inactive');
+    
+
+//     const productDetailClose = document.createElement('div');
+//     productDetailClose.classList.add('product-detail-close');
+//     const iconClose = document.createElement('img');
+//     iconClose.setAttribute('src','./icons/icon_close.png' );
+
+//     const imagenDetailProduct = document.createElement('img');
+//     imagenDetailProduct.setAttribute('src',product.image );
+//     console.log("img",product.image);
+
+//     const productInfo2 = document.createElement('div');
+//     productInfo2.classList.add('product-info2');
+//     const priceDetail2 = document.createElement('p');
+//     priceDetail2.innerText = product.price;
+//     const nameDetail2 = document.createElement('p');
+//     nameDetail2.innerText = product.name;
+//     const descriptionOfProduct = document.createElement('p');
+//     descriptionOfProduct.innerText = product.description;
+//     const buttonCart = document.createElement('button');
+//     buttonCart.classList.add('primary-button', 'add-to-cart-button');
+//     const imgInButton = document.createElement("img");
+//     imgInButton.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+//     //Ubicar ahora cada elemnto dentro de cada contenedor correspondiente
+//     buttonCart.appendChild(imgInButton);//imagen dentro de button
+//     //button,precio, descripcion, y nombre dentro de productInfo2(div)
+//     productInfo2.appendChild(buttonCart);
+//     productInfo2.appendChild(descriptionOfProduct);
+//     productInfo2.appendChild(nameDetail2);
+//     productInfo2.appendChild(priceDetail2);
+//     //imagen dentro de product-deatil-close(div)
+//     productDetailClose.appendChild(iconClose);
+//     //product-info, imagenDetailProduct, productDetailClose dentro del aside
+//     asideProducDetail2.appendChild(productInfo2);
+//     asideProducDetail2.appendChild(imagenDetailProduct);
+//     asideProducDetail2.appendChild(productDetailClose);
+//     productDeatil2.appendChild(asideProducDetail2);    
+
+// }
+
+// }
+// detallesProductos(productList);
+
+// const showProductsOnScreen= (lista)=>{ //crea los productos en el menu
+    
+//     lista.forEach(product => { //recorre cada elemento del arreglo
+//         const productDetail2= document.createElement('div');
+//         productDetail2.classList.add('product-detail2');
+
+//         productDetail2.addEventListener('click',()=>{ //evento de escucha
+//             openProductInfo(product); //llamo a la funcion
+
+//         })
+
+//     const asideProducDetail2 = document.createElement('aside');
+//     asideProducDetail2.classList.add('product-detail2');
+
+//     const productDetailClose = document.createElement('div');
+//     productDetailClose.classList.add('product-detail-close');
+//     const iconClose = document.createElement('img');
+//     iconClose.setAttribute('src','./icons/icon_close.png' );
+
+//     const imagenDetailProduct = document.createElement('img');
+//     imagenDetailProduct.setAttribute('src',product.image );
+
+//     const productInfo2 = document.createElement('div');
+//     productInfo2.classList.add('product-info2');
+//     const priceDetail2 = document.createElement('p');
+//     priceDetail2.innerText = product.price;
+//     const nameDetail2 = document.createElement('p');
+//     nameDetail2.innerText = product.name;
+//     const descriptionOfProduct = document.createElement('p');
+//     descriptionOfProduct.innerText = product.description;
+//     const buttonCart = document.createElement('button');
+//     buttonCart.classList.add('primary-button', 'add-to-cart-button');
+//     const imgInButton = document.createElement("img");
+//     imgInButton.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+//     //Ubicar ahora cada elemnto dentro de cada contenedor correspondiente
+//     buttonCart.appendChild(imgInButton);//imagen dentro de button
+//     //button,precio, descripcion, y nombre dentro de productInfo2(div)
+//     productInfo2.appendChild(buttonCart);
+//     productInfo2.appendChild(descriptionOfProduct);
+//     productInfo2.appendChild(nameDetail2);
+//     productInfo2.appendChild(priceDetail2);
+//     //imagen dentro de product-deatil-close(div)
+//     productDetailClose.appendChild(iconClose);
+//     //product-info, imagenDetailProduct, productDetailClose dentro del aside
+//     asideProducDetail2.appendChild(productInfo2);
+//     asideProducDetail2.appendChild(imagenDetailProduct);
+//     asideProducDetail2.appendChild(productDetailClose);
+
+// });
+
+// const openProductInfo = (product)=>{ //muestra el aside con la info del producto seleccionado
+//     const aside = document.querySelector('.product-detail2');
+//     const productImg = document.querySelector('.product-detail2 > img:nth-child(2)');
+//     const productPrice = document.querySelector('.product-info2 .product-price');
+//     const productName= document.querySelector('.product-info2 .product-name');
+       
+//         productImg.setAttribute('src',product.img);
+//         productPrice.textContent= product.price;
+//         productName.textContent= product.name;
+
+
+// }
+// }
